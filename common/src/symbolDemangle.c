bd9b7015b62ed7217ff2d0c92161f4e275db6bf1
James A. Kupsch <kupsch@cs.wisc.edu>
2020-10-16 11:07:04 -0500

    udpate comment

--- common/src/symbolDemangle.c
+++ common/src/symbolDemangle.c
@@ -36,13 +36,13 @@
 
 
 // Returns a malloc'd string that is the demangled symbol name.  THe caller is
-// responsible for the freeing this memory, return NULL on failure.  The symbol
-// name symName is demangled using the cplus_demangle function after first
-// removing any versioning suffixes (first '@') or stabs suffixes (first ':')
-// to create the mangled name.  If cplus_demangle fails, the mangled name is
-// returned unmodified.  If cplus_demangle succeeds and includeParams is false,
-// then any clone suffixes (the first '.' to the end of the mangled name) are
-// appended to the value from cplus_demangle.
+// responsible for the freeing this memory.  Returns NULL on malloc failure.
+// The symbol name symName is demangled using the cplus_demangle function after
+// first removing any versioning suffixes (first '@') or stabs suffixes (first
+// ':') to create the mangled name.  If cplus_demangle fails, the mangled name
+// is returned unmodified.  If cplus_demangle succeeds and includeParams is
+// false, then any clone suffixes (the first '.' to the end of the mangled
+// name) are appended to the value from cplus_demangle.
 //
 // Other than the removal of versioning and stabs suffixes, and appending any
 // clone suffixes if includeParams is false to the result, the result should be
