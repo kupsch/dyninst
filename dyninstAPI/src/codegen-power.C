c88b4c344ffa2906925ac3c1c792eb1c5fc4dc95
James A. Kupsch <kupsch@cs.wisc.edu>
2021-07-19 13:42:43 -0500

    fix duplicate branch condition by removing branch
    else if condition happens to be identical to the if condition due
    to constant identifiers having the same value.  The tests should
    be made more specific to distinguish the two cases or the else if
    should be removed.

--- dyninstAPI/src/codegen-power.C
+++ dyninstAPI/src/codegen-power.C
@@ -955,8 +955,16 @@ void insnCodeGen::generateSimple(codeGen &gen, int op,
   XFORM_RB_SET(insn, src2);
   if (op==ANDop) {
       xop=ANDxop;
+  /*
   } else if (op==ORop) {
       xop=ORxop;
+  
+  *  FIX ME: The above "else if" condition and code are commented out to remove
+  *  a duplicate branch condition as both ANDop and ORop have the same value.
+  *  This implies that the assignment in this branch is never executed.
+  *  Further tests to distinguish between the AND and OR op are needed or this
+  *  code should be eliminated
+  */
   } else {
       // only AND and OR are currently designed to use genSimpleInsn
       assert(0);
