1152aaae016082d2f6de2a55a473454ad54b0a89
James A. Kupsch <kupsch@cs.wisc.edu>
2026-08-04 11:20:58 -0500

    cleanup: renameSection
    - pass in only the old name, the new name is computed consistently
      in this function (2nd char changed to 'o')
    - remove renameAll parameter, as it was always false

--- symtabAPI/src/emitElf.C
+++ symtabAPI/src/emitElf.C
@@ -355,16 +355,14 @@ typename emitElf<ElfTypes>::Elf_Off emitElf<ElfTypes>::findLastLoadableSec() {
     return lastLoadableSecStart;
 }
 
-// Rename an old section. Lengths of old and new names must match.
-// Only renames the FIRST matching section encountered.
+// Renames 1st oldName section by changing 2nd char to 'o'
 template<class ElfTypes>
-void emitElf<ElfTypes>::renameSection(const std::string &oldStr, const std::string &newStr, bool renameAll) {
-    assert(oldStr.length() == newStr.length());
-    for (unsigned k = 0; k < secNames.size(); k++) {
-        if (secNames[k] == oldStr) {
-            secNames[k].replace(0, oldStr.length(), newStr);
-            if (!renameAll)
-                break;
+void emitElf<ElfTypes>::renameSection(const std::string &oldName) {
+    assert(oldName.length() >= 2);
+    for (auto &secName : secNames)  {
+        if (secName == oldName)  {
+            secName[1] = 'o';
+            break;
         }
     }
 }
@@ -535,9 +533,7 @@ bool emitElf<ElfTypes>::driver(std::string fName) {
                 (shdr->sh_addr >= moveSecAddrRange[i][0] && shdr->sh_addr < moveSecAddrRange[i][1])) {
                 newshdr->sh_type = SHT_PROGBITS;
                 changeMapping[sectionNumber] = 1;
-                string newName = ".o";
-                newName.append(name, 2, strlen(name));
-                renameSection(name, newName, false);
+                renameSection(name);
             }
         }
 
@@ -571,9 +567,7 @@ bool emitElf<ElfTypes>::driver(std::string fName) {
             // Change the data to update the relocation addr
             newshdr->sh_type = SHT_PROGBITS;
             changeMapping[sectionNumber] = 1;
-            string newName = ".o";
-            newName.append(name, 2, strlen(name));
-            renameSection(name, newName, false);
+            renameSection(name);
         }
 
         // Only need to rewrite data section
@@ -581,16 +575,11 @@ bool emitElf<ElfTypes>::driver(std::string fName) {
             && foundSec->getRegionType() == Region::RT_DATA) {
             // Clear TLS flag
             newshdr->sh_flags &= ~SHF_TLS;
-
-            string newName = ".o";
-            newName.append(name, 2, strlen(name));
-            renameSection(name, newName, false);
+            renameSection(name);
         }
 
         if (isStaticBinary && ((strcmp(name, ".rel.plt") == 0) || (strcmp(name, ".rela.plt") == 0 ))) {
-            string newName = ".o";
-            newName.append(name, 2, strlen(name));
-            renameSection(name, newName, false);
+            renameSection(name);
             // The old sections are no longer REL or RELA, change to PROGBITS
             newshdr->sh_type = SHT_PROGBITS;
 
